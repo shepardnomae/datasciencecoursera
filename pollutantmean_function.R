51393bc9de353f69dff37c6f6babacefa3ce051d
ToNg KIMU <tongkimu@ToNgs-MacBook-Pro.local>
2021-01-18 22:34:08 +0900

    dir, pull data done

--- pollutantmean_function.R
+++ pollutantmean_function.R
@@ -1,3 +1,10 @@
 pollutantmean <- function(x) {if (x == "sulfate")
   {mean(a$sulfate[!is.na(a$sulfate)])}
-  else {mean(a$nitrate[!is.na(a$nitrate)])}}
+    else {mean(a$nitrate[!is.na(a$nitrate)])}}
+pollu1 <- function(x) {setwd(x)}
+readx <- function(x) {list.files(pattern="*.csv")[x]}
+  
+  do.call(rbind, lapply(temp, function(y) read.csv(y, stringsAsFactors = FALSE)))
+  
+ready <- function(x) { temp <- list.files(pattern="*.csv")[x];
+  do.call(rbind, lapply(temp, function(y) read.csv(y, stringsAsFactors = FALSE)))}
